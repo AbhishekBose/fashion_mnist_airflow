98ae1f22fcd03a58da563050230b7096fc03825d
AbhishekBose <abose550@gmail.com>
2019-10-31 01:21:29 +0530

    trainig tested

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ class LossHistory(keras.callbacks.Callback):
 def train(trainX,trainY,testX,testY):
     model = model_2()
     history = LossHistory()
-    model.fit(trainX,trainY, batch_size=data['batch_size'], epochs=data['epochs'], verbose=data['verbose'], callbacks=[history])
+    model.fit(trainX,trainY, batch_size=data['batch_size'], epochs=data['epochs'], verbose=data['verbose'],validation_data=(testX,testY) ,callbacks=[history])
 #%%
 if __name__ == "__main__":
     trainX = np.load('/home/abhishek/fashion_mnist_airflow/data/0000/X_train.npy')
