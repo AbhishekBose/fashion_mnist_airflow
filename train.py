d7409bd709b1f590139a63c37bcd72affef052af
AbhishekBose <abose550@gmail.com>
2019-11-08 02:19:54 +0530

    logged db_ops and model.py file as an artifact

--- train.py
+++ train.py
@@ -41,43 +41,51 @@ if __name__ == "__main__":
 
     coll_name = 'dataset'
     mq = mongoQueue(coll_name)
-    parent_path = '/home/abhishek/fashion_mnist_airflow/'
+    model = model_2(data['opt'])
+    print(os.getcwd())
+    # parent_path = '/home/abhishek/fashion_mnist_airflow/'
     dataset_count = 0
     with mlflow.start_run(run_name='fashion_mnist'):
         while mq.Dequeue !=None:
-            dataset_info = mq.Dequeue()
-            dataset_id = dataset_info['dataset_id']
-            # with mlflow.start_run():
-            mq.setAsProcessing(dataset_id)
-            model = model_2(data['opt'])
-            train_X_path = dataset_info['path']
-            train_Y_path = train_X_path.replace('X','Y')
-            trainX = np.load(parent_path+train_X_path)
-            trainY = np.load(parent_path+train_Y_path)
-            # trainX = np.load('/home/abhishek/fashion_mnist_airflow/data/0000/X_train.npy')
-            # trainY = np.load('/home/abhishek/fashion_mnist_airflow/data/0000/Y_train.npy')
+            try:
+                dataset_info = mq.Dequeue()
+                dataset_id = dataset_info['dataset_id']
+                # with mlflow.start_run():
+                mq.setAsProcessing(dataset_id)
+                train_X_path = dataset_info['path']
+                train_Y_path = train_X_path.replace('X','Y')
+                trainX = np.load(train_X_path)
+                trainY = np.load(train_Y_path)
+                # trainX = np.load('/home/abhishek/fashion_mnist_airflow/data/0000/X_train.npy')
+                # trainY = np.load('/home/abhishek/fashion_mnist_airflow/data/0000/Y_train.npy')
 
-            testX = np.load('/home/abhishek/fashion_mnist_airflow/data/testing/X_test.npy')
-            testY = np.load('/home/abhishek/fashion_mnist_airflow/data/testing/Y_test.npy')
-            trained_model = train(trainX,trainY,testX,testY,model)
-            scores = model.evaluate(testX,testY,verbose=1)
-            print(scores)
-            X_train_dim = trainX.shape
-            Y_train_dim = trainY.shape
+                testX = np.load('data/testing/X_test.npy')
+                testY = np.load('data/testing/Y_test.npy')
+                trained_model = train(trainX,trainY,testX,testY,model)
+                scores = model.evaluate(testX,testY,verbose=1)
+                print(scores)
+                X_train_dim = trainX.shape
+                Y_train_dim = trainY.shape
 
-            X_test_dim = testX.shape
-            Y_test_dim = testY.shape
+                X_test_dim = testX.shape
+                Y_test_dim = testY.shape
 
-            mlflow.log_param("alpha",0.001)
-            mlflow.log_param("epochs",data['epochs'])
-            mlflow.log_param('optimizer',data['opt'])
-            mlflow.log_param("batch_size",data['batch_size'])
-            mlflow.log_metric("eval_loss",scores[0])
-            mlflow.log_metric("eval_acc",scores[1])
-            mlflow.log_metric("eval_precision",scores[2])
-            mlflow.log_metric("eval_recall",scores[3])
-            mlflow.log_metric(key="accuracy", value=scores[1], step=dataset_count)
-            dataset_count+=1
-            mq.setAsProcessed(dataset_id)
+                mlflow.log_param("alpha",0.001)
+                mlflow.log_param("epochs",data['epochs'])
+                mlflow.log_param('optimizer',data['opt'])
+                mlflow.log_param("batch_size",data['batch_size'])
+                mlflow.log_metric("eval_loss",scores[0])
+                mlflow.log_metric("eval_acc",scores[1])
+                mlflow.log_metric("eval_precision",scores[2])
+                mlflow.log_metric("eval_recall",scores[3])
+                mlflow.log_metric(key="accuracy", value=scores[1], step=dataset_count)
+            
+                dataset_count+=1
+                mq.setAsProcessed(dataset_id)
+            except Exception as e:
+                print('Exception encountered is:: ',str(e))
+                break
+        mlflow.log_artifact('model/model.py')
+        mlflow.log_artifact('db/db_ops.py')
     
         
